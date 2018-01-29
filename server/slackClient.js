4d520fd995b17fafb52fcfc9ca2a7ef3fbc0bd12
Jonathan Vargas <jonathanv3232@gmail.com>
2018-01-29 14:38:55 -0500

    slack bot sends a message when there is a message sent to a channel the bot is in

--- server/slackClient.js
+++ server/slackClient.js
@@ -1,8 +1,16 @@
 'use strict';
-const { RtmClient, CLIENT_EVENTS } = require('@slack/client');
-
+const { RtmClient, CLIENT_EVENTS, RTM_EVENTS } = require('@slack/client');
+let rtm = null;
 function logOnAuthentication(rtmStartData) {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+}
+
+function handleOnMessage(message) {
+  console.log(message);
+  rtm.sendMessage('wus good!!!', message.channel)
+    // Returns a promise that resolves when the message is sent
+    .then(() => console.log(`Message sent`))
+    .catch(console.error);
 }
 
 function hasAuthenticated(rtm, handler) {
@@ -10,8 +18,9 @@ function hasAuthenticated(rtm, handler) {
 }
 
 module.exports.init = function slackClient(token, logLevel) {
-  const rtm = new RtmClient(token, { logLevel: logLevel });
+  rtm = new RtmClient(token, { logLevel: logLevel });
   hasAuthenticated(rtm, logOnAuthentication);
+  rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
   return rtm;
 };
 
