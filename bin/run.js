41fe2d807484b1e5a94f66be8b43cbc8c25cd2e5
Jonathan Vargas <jonathanv3232@gmail.com>
2018-01-29 14:07:51 -0500

    bot now connects to slack and is online

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,19 @@
 'use strict';
+
 require('dotenv').config();
+const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
-
 const server = http.createServer(service);
 
-server.listen(3000);
+const slackToken = process.env.SLACK_TOKEN;
+const slackLogLevel = 'verbose';
+
+const rtm = slackClient.init(slackToken, slackLogLevel);
+
+rtm.start();
+
+slackClient.hasAuthenticated(rtm, () => server.listen(3000));
 
 server.on('listening', function() {
   console.log(`listening on port ${server.address().port} in ${service.get('env')} mode`);
