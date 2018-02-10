e332f86ce6fb9ab5a11136450c811811aba79e0d
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-10 13:49:22 -0500

    sets up service registry to the architecture

--- server/service.js
+++ server/service.js
@@ -3,4 +3,13 @@
 const express = require('express');
 const service = express();
 
+service.put('/service/:intent/:port', (req, res, next) => {
+  const newService = req.params.intent;
+  const port = req.params.port;
+  const remoteIp = req.connection.remoteAddress.includes('::')
+    ? `[${req.connection.remoteAddress}]`
+    : req.connection.remoteAddress;
+  res.json({ result: `${newService} will be added to ${remoteIp}:${port}` });
+});
+
 module.exports = service;
